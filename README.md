d62ce760ccdd7d19e4d6fd9a7902ea3a7e887eb2
jdv <jdv@bunsentop.Home>
2020-10-21 08:25:25 -0400

    Updated README

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # goSheets
+## Example of writing to a sheet
+```
+package main
 
-## For an example of how to post to Google Sheets, see main.go
+import (
+	"fmt"
+
+	auth "github.com/jdvober/goGoogleAuth"
+	sh "github.com/jdvober/goSheets/values"
+)
+
+func main() {
+	client := auth.Authorize()
+
+	spreadsheetId := "1HRfK4yZERLWd-OcDZ8pJRirdzdkHln3SUtIfyGZEjNk"
+	rangeData := "sheet2!I1:J3"
+	values := [][]interface{}{{"sample_A1", "sample_B1"}, {"sample_A2", "sample_B2"}, {"sample_A3", "sample_A3"}}
+
+	sh.BatchUpdate(client, spreadsheetId, rangeData, values)
+}
+```
 ## Google's Reference for the Sheets API can be found at https://developers.google.com/sheets/api/samples#recipes
--- main.go
+++ main.go
@@ -1,21 +1,3 @@
-/* package main
- *
- * import (
- *     auth "github.com/jdvober/goGoogleAuth"
- *     sh "github.com/jdvober/goSheets/values"
- * )
- *
- *
- * func main() {
- *     spreadsheetID := "1HRfK4yZERLWd-OcDZ8pJRirdzdkHln3SUtIfyGZEjNk"
- *     r := "H4"
- *     // With range = "A1"
- *     vals := [][]interface{}{{"sample_A1", "sample_B1"}, {"sample_A2", "sample_B2"}, {"sample_A3", "sample_A3"}}
- *     client := auth.Authorize()
- *     sh.BatchUpdate(client, spreadsheetID, r, vals)
- *
- * } */
-
 package main
 
 import (
@@ -24,14 +6,6 @@ import (
 	auth "github.com/jdvober/goGoogleAuth"
 	sh "github.com/jdvober/goSheets/values"
 )
-
-type body struct {
-	Data struct {
-		Range  string     `json:"range"`
-		Values [][]string `json:"values"`
-	} `json:"data"`
-	ValueInputOption string `json:"valueInputOption"`
-}
 
 func main() {
 	client := auth.Authorize()
